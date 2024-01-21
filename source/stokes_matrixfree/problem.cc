083ca2c3bc1def70ca8497b7cb9b96c76b4e59ca
Marc Fehling <mafehling.git@gmail.com>
2024-01-21 15:08:19 -0700

    Time of constraints distribution.

--- source/stokes_matrixfree/problem.cc
+++ source/stokes_matrixfree/problem.cc
@@ -362,95 +362,108 @@ namespace StokesMatrixFree
   void
   Problem<dim, LinearAlgebra, spacedim>::solve()
   {
-    TimerOutput::Scope t(getTimer(), "solve");
-
-    // We need to introduce a vector that does not contain all ghost elements.
     typename LinearAlgebra::BlockVector completely_distributed_solution;
-    stokes_operator->initialize_dof_vector(completely_distributed_solution);
 
-    SolverControl solver_control_refined(system_rhs.size(),
-                                         prm.solver_tolerance_factor * system_rhs.l2_norm());
+    {
+      TimerOutput::Scope t(getTimer(), "solve");
 
-    if (prm.solver_type == "AMG")
-      {
-        solve_amg<dim, LinearAlgebra, spacedim>(solver_control_refined,
-                                                *stokes_operator,
-                                                *a_block_operator,
-                                                *schur_block_operator,
-                                                completely_distributed_solution,
-                                                system_rhs);
-      }
-    else if (prm.solver_type == "GMG")
-      {
-        const std::string filename_mg_level =
-          filename_stem + "-mglevel-cycle_" + std::to_string(cycle) + ".log";
+      // We need to introduce a vector that does not contain all ghost elements.
+      stokes_operator->initialize_dof_vector(completely_distributed_solution);
 
-        if (prm.prm_multigrid.smoother_preconditioner_type == "Extended Diagonal")
-          {
-            solve_gmg<PreconditionExtendedDiagonal<typename LinearAlgebra::Vector>,
-                      dim,
-                      LinearAlgebra,
-                      spacedim>(solver_control_refined,
-                                *stokes_operator,
-                                *a_block_operator,
-                                *schur_block_operator,
-                                completely_distributed_solution,
-                                system_rhs,
-                                prm.prm_multigrid,
-                                mapping_collection,
-                                quadrature_collection_v,
-                                dof_handlers,
-                                filename_mg_level);
-          }
-        else if (prm.prm_multigrid.smoother_preconditioner_type == "ASM")
-          {
-            solve_gmg<PreconditionASM<typename LinearAlgebra::Vector>,
-                      dim,
-                      LinearAlgebra,
-                      spacedim>(solver_control_refined,
-                                *stokes_operator,
-                                *a_block_operator,
-                                *schur_block_operator,
-                                completely_distributed_solution,
-                                system_rhs,
-                                prm.prm_multigrid,
-                                mapping_collection,
-                                quadrature_collection_v,
-                                dof_handlers,
-                                filename_mg_level);
-          }
-        else if (prm.prm_multigrid.smoother_preconditioner_type == "Diagonal")
-          {
-            solve_gmg<DiagonalMatrix<typename LinearAlgebra::Vector>, dim, LinearAlgebra, spacedim>(
-              solver_control_refined,
-              *stokes_operator,
-              *a_block_operator,
-              *schur_block_operator,
-              completely_distributed_solution,
-              system_rhs,
-              prm.prm_multigrid,
-              mapping_collection,
-              quadrature_collection_v,
-              dof_handlers,
-              filename_mg_level);
-          }
-        else
-          {
-            AssertThrow(false, ExcNotImplemented());
-          }
-      }
-    else
-      {
-        Assert(false, ExcNotImplemented());
-      }
+      SolverControl solver_control_refined(system_rhs.size(),
+                                           prm.solver_tolerance_factor * system_rhs.l2_norm());
 
-    Log::log_iterations(solver_control_refined);
+      if (prm.solver_type == "AMG")
+        {
+          solve_amg<dim, LinearAlgebra, spacedim>(solver_control_refined,
+                                                  *stokes_operator,
+                                                  *a_block_operator,
+                                                  *schur_block_operator,
+                                                  completely_distributed_solution,
+                                                  system_rhs);
+        }
+      else if (prm.solver_type == "GMG")
+        {
+          const std::string filename_mg_level =
+            filename_stem + "-mglevel-cycle_" + std::to_string(cycle) + ".log";
 
-    constraints_v.distribute(completely_distributed_solution.block(0));
-    constraints_p.distribute(completely_distributed_solution.block(1));
+          if (prm.prm_multigrid.smoother_preconditioner_type == "Extended Diagonal")
+            {
+              solve_gmg<PreconditionExtendedDiagonal<typename LinearAlgebra::Vector>,
+                        dim,
+                        LinearAlgebra,
+                        spacedim>(solver_control_refined,
+                                  *stokes_operator,
+                                  *a_block_operator,
+                                  *schur_block_operator,
+                                  completely_distributed_solution,
+                                  system_rhs,
+                                  prm.prm_multigrid,
+                                  mapping_collection,
+                                  quadrature_collection_v,
+                                  dof_handlers,
+                                  filename_mg_level);
+            }
+          else if (prm.prm_multigrid.smoother_preconditioner_type == "ASM")
+            {
+              solve_gmg<PreconditionASM<typename LinearAlgebra::Vector>,
+                        dim,
+                        LinearAlgebra,
+                        spacedim>(solver_control_refined,
+                                  *stokes_operator,
+                                  *a_block_operator,
+                                  *schur_block_operator,
+                                  completely_distributed_solution,
+                                  system_rhs,
+                                  prm.prm_multigrid,
+                                  mapping_collection,
+                                  quadrature_collection_v,
+                                  dof_handlers,
+                                  filename_mg_level);
+            }
+          else if (prm.prm_multigrid.smoother_preconditioner_type == "Diagonal")
+            {
+              solve_gmg<DiagonalMatrix<typename LinearAlgebra::Vector>,
+                        dim,
+                        LinearAlgebra,
+                        spacedim>(solver_control_refined,
+                                  *stokes_operator,
+                                  *a_block_operator,
+                                  *schur_block_operator,
+                                  completely_distributed_solution,
+                                  system_rhs,
+                                  prm.prm_multigrid,
+                                  mapping_collection,
+                                  quadrature_collection_v,
+                                  dof_handlers,
+                                  filename_mg_level);
+            }
+          else
+            {
+              AssertThrow(false, ExcNotImplemented());
+            }
+        }
+      else
+        {
+          Assert(false, ExcNotImplemented());
+        }
 
-    locally_relevant_solution = completely_distributed_solution;
-    locally_relevant_solution.update_ghost_values();
+      Log::log_iterations(solver_control_refined);
+    }
+
+    {
+      TimerOutput::Scope t(getTimer(), "distribute_constraints");
+
+      constraints_v.distribute(completely_distributed_solution.block(0));
+      constraints_p.distribute(completely_distributed_solution.block(1));
+    }
+
+    {
+      TimerOutput::Scope t(getTimer(), "update_ghost_values");
+
+      locally_relevant_solution = completely_distributed_solution;
+      locally_relevant_solution.update_ghost_values();
+    }
 
 
 
